223b8d9f12fb7eb2a6539abc220952da5387f52a
Harry Wang <hueyjie2@illinois.edu>
2023-08-31 21:43:55 -0500

    Summer 2023 Update

---
+++ assets/ssa-data.js
@@ -0,0 +1,78 @@
+// GLOBAL IMPORTED DATA FILE
+// allows us to circumvent hosting CORS policy, and not have to meddle with html directly to update things
+
+// ===== event list formatting =====
+// name,date,location,registration form link
+//
+// if event is not yet open for registration, leave out the registration form link
+
+var eventslist_csv = `
+Labor Day Weekend Chicago Trip,2-3 September 2023,Chicago,closed
+Friendsgiving,November 2023,On-Campus
+Chinese New Year,10 February 2023,On-Campus
+Spring Informal,Late April 2023,On-Campus
+Illini Day,August 2024,Singapore
+`;
+
+// ===== resources formatting =====
+// name,url
+//
+// to be written in whatever order you want the list to appear in
+var resourceslist_csv = `
+UI Course Explorer,https://courses.illinois.edu
+UI Course Explorer (CS),https://courses.illinois.edu/schedule/default/default/CS
+UI Course Explorer (ECE),https://courses.illinois.edu/schedule/default/default/ECE
+Course Explorer (MechE),https://courses.illinois.edu/schedule/default/default/ME
+Degree Progression (CS),http://catalog.illinois.edu/undergraduate/engineering/computer-science-bs/#degreerequirementstext
+Degree Progression (ECE),http://catalog.illinois.edu/undergraduate/engineering/computer-engineering-bs/#degreerequirementstext
+Degree Progression (MechE),http://catalog.illinois.edu/undergraduate/engineering/mechanical-engineering-bs/#degreerequirementstext
+MTD's Learn to Ride,https://mtd.org/maps-and-schedules/uiuc-learn-to-ride
+`;
+
+
+// ===== exco list formatting =====
+// name,image path,email (use br if necessary)
+//
+// to be written in the order of president/vp external/vp internal/treasurer/secretary
+var excolist_csv = `
+Harry Wang,assets/img/exco23/harry.jpg,<b>hueyjie2</b> at illinois dot edu<br><b>president</b> at uiucssa dot com
+Parithimaal Karmehan,assets/img/exco23/pari.jpg,<b>pk38</b> at illinois dot edu<br><b>external</b> at uiucssa dot com
+Andrew Loh,assets/img/exco23/andrew.jpg,<b>acloh2</b> at illinois dot edu<br><b>internal</b> at uiucssa dot com
+Adithya Balaji,assets/img/exco23/balarizz.jpg,<b>abalaji5</b> at illinois dot edu<br><b>treasurer</b> at uiucssa dot com
+Aaditya Kothary,assets/img/exco23/aadi.jpg,<b>kothary3</b> at illinois dot edu<br><b>secretary</b> at uiucssa dot com
+`;
+
+// ===== previous year exco formatting =====
+// year,president,vp external,vp internal,treasurer,secretary,social chair
+//
+// to be written in whatever order you want the list to appear in
+var prevexcolist_csv = `
+2022-23,Jennifer Lahey,May Lee,Hanpu Liu,Davis Zhang,Ethan Handojo
+2021-22,Ravyu Sivakumaran,Faith Tan,Advay Sudarshan,Nairen Fu,Megan Poh,Vignesh Ravibaskar
+2020-21,Leck Kirk Shen,Liu Zhu Xuan,Lydia Ng,Daniel Lee
+2019-20,Goh Hao Wei,Ho Yi Shian,Tan Yong Zhi
+2018-19,Koh Hui Yi,Ang Wei Yang,Han Jia Ling
+`;
+
+// ===== gallery image list formatting =====
+// url,left caption,right caption
+//
+// to be written in whatever order you want the images to appear in
+var gallery_csv = `
+assets/img/gallery/2023-spring-informal-3.jpeg,<b>Spring Informal</b> | On-Campus,29 Apr 2023
+assets/img/gallery/2023-spring-informal-2.jpeg,<b>Spring Informal</b> | On-Campus,29 Apr 2023
+assets/img/gallery/2023-spring-informal.jpeg,<b>Spring Informal</b> | On-Campus,29 Apr 2023
+assets/img/gallery/2022-chicago-outlets.jpeg,<b>Chicago Trip</b> | Chicago Premium Outlets,6 Sep 2022
+assets/img/gallery/2022-chicago-bean.JPG,<b>Chicago Trip</b> | The Bean,5 Sep 2022
+assets/img/gallery/2022-illini-day.jpeg,<b>Illini Day</b> | Marina Barrage,5 Aug 2022
+assets/img/gallery/2018-chicago-outlets.jpg,<b>Chicago Trip</b> | Chicago Premium Outlets,2 Sep 2018
+assets/img/gallery/2017-midwest-games.jpg,<b>Midwest Games</b>,2017
+assets/img/gallery/2017-cny.jpg,<b>Chinese New Year</b>,2017
+assets/img/gallery/2016-thanksgiving-dinner.jpg,<b>Thanksgiving Dinner</b>,2016
+assets/img/gallery/2016-chicago-outlets.jpg,<b>Chicago Trip</b>,2016
+assets/img/gallery/2016-chicago-bean.jpg,<b>Chicago Trip</b>,2016
+assets/img/gallery/2016-bbq.jpg,<b>Singapore BBQ</b>,2016
+`;
+
+// --- place unused image entries here ---
+// assets/img/gallery/2019-chicago-outlets.png,<b>Chicago Trip</b> | Chicago Premium Outlets,1 Sep 2019
---
+++ main.js
@@ -0,0 +1,195 @@
+// todo
+// progress bar on gallery?
+// auto image next
+
+function splitCSV(csv, removeEmptyFields = true) {
+	return csv.trim().split("\n").filter(line => line.length > 0).map(line => line.trim().split(",").map(field => field.trim()).filter(field => !removeEmptyFields || field.length > 0));
+}
+
+// loads all the dynamically populated data from ssa-data.js
+var galleryAutoIntervalID = "";
+function loadAllData() {
+	loadEventList();
+	loadResourcesList();
+	loadGalleryImages();
+	loadExcoList();
+	loadPreviousExcoList();
+	galleryAutoIntervalID = window.setInterval(carouselNextImage, 7000);
+}
+
+// loads the upcoming events data
+function loadEventList() {
+	
+	// generate event cards
+	// <div class="event-card">
+	// 	<h4>Event Name</h4>
+	// 	<p>Event Date</p>
+	// 	<p>Event Location</p>
+	// 	<p>Registration Link</p>
+	// </div>
+	// csv: name,date,location,registration form link (if blank, rendered as "not yet")
+	var events_list = splitCSV(eventslist_csv);
+	var eventcards_html = "", curr_event = [];
+	for (var pos = 0; pos < events_list.length; pos += 1) {
+		curr_event = events_list[pos];
+		if (curr_event.length < 3) {
+			console.log(`malformed event entry: ${curr_event}`);
+			continue;
+		}
+		eventcards_html += `<div class='event-card'><h4>${curr_event[0]}</h4><p>${curr_event[1]}</p><p><i>${curr_event[2]}</i></p>${ curr_event.length > 3 ? curr_event[3] == "closed" ? "<p style='color: red;'>Registration closed</p>" : `<a href='${curr_event[3]}'><p>Click for Registration Form</p></a>` : "<p style='color: orange;'>Registration not yet open</p>" }</div>`;
+	}
+
+	// write out
+	document.getElementById("event-cards").innerHTML = eventcards_html;
+}
+
+// loads the resource links data
+function loadResourcesList() {
+
+	// generate resource link cards
+	// <a class="resource-link" href="">link 1</a>
+	// csv: name,url
+	var resources_list = splitCSV(resourceslist_csv);
+	var resourcecards_html = "", curr_resource = [];
+	for (var pos = 0; pos < resources_list.length; pos += 1) {
+		curr_resource = resources_list[pos];
+		if (curr_resource.length < 2) {
+			console.log(`malformed resource entry: ${curr_resource}`);
+			continue;
+		}
+		resourcecards_html += `<a class='resource-link' href='${curr_resource[1]}'>${curr_resource[0]}</a>`;
+	}
+
+	// write out
+	document.getElementById("resource-links").innerHTML = resourcecards_html;
+}
+
+// loads the current exco data
+function loadExcoList() {
+
+	// generate exco cards
+	// <div class="person">
+	// 	<img class="portrait" src="assets/img/head.jpg"style="border-color: red/orange/teal;">
+	// 	<span>
+	// 		<b>Name Name</b>
+	// 		<br>
+	// 		Position
+	// 	</span>
+	// 	<code>
+	// 		<b>NetID</b> at illinois dot edu
+	// 		<br>
+	// 		<b>position</b> at uiucssa dot com
+	// 	</code>
+	// </div>
+	// csv: name,image path,email (use br if necessary)
+	var exco_list = splitCSV(excolist_csv);
+	var excocards_html = "", curr_member = [];
+	var exco_roles = [["President", "red"], ["Vice President (External)", "orange"], ["Vice President (Internal)", "orange"], ["Treasurer", "teal"], ["Secretary", "teal"]];
+	for (var pos = 0; pos < exco_list.length; pos += 1) {
+		curr_member = exco_list[pos];
+		if (curr_member.length < 3) {
+			console.log(`malformed exco entry: ${curr_member}`);
+			continue;
+		}
+		excocards_html += `<div class="person"><img class="portrait" src="${curr_member[1]}"style="border-color: ${exco_roles[pos][1]};"><span><b>${curr_member[0]}</b><br>${exco_roles[pos][0]}</span><code>${curr_member[2]}</code></div>`;
+	}
+
+	// write out
+	document.getElementById("exco").innerHTML = excocards_html;
+}
+
+// loads the past exco data
+function loadPreviousExcoList() {
+
+	// generate table listing, one row for each line
+	// csv: year,president,vp external,vp internal,treasurer,secretary,social chair
+	var prevexco_list = splitCSV(prevexcolist_csv, false);
+	var prevexcotable_html = "<table><tr><th>Year</th><th>President</th><th>Vice President (External)</th><th>Vice President (Internal)</th><th>Treasurer</th><th>Secretary</th><th>Social Chair</th></tr>", curr_year = [];
+	for (var pos = 0; pos < prevexco_list.length; pos += 1) {
+		curr_year = prevexco_list[pos];
+		prevexcotable_html += `<tr>`;
+		curr_year.forEach(member => prevexcotable_html += `<td>${member}</td>`);
+		prevexcotable_html += `</tr>`;
+	}
+
+	// write out
+	document.getElementById("prev-exco-table").innerHTML = prevexcotable_html + "</table>";
+}
+
+// loads the images in the gallery
+var gallery_data = { images: [], idx: 0 };
+function loadGalleryImages() {
+
+	// repopulate global variable galleryimages_list
+	// csv: url,left caption,right caption
+	gallery_data.images = splitCSV(gallery_csv);
+	gallery_data.idx = 0;
+
+	// render first image
+	carouselRenderImage();
+}
+
+// renders the current image as specified in gallery_data
+async function carouselRenderImage() {
+
+	// get elements
+	var ele_img = document.getElementById("carousel-img");
+	var ele_lcap = document.getElementById("carousel-captionL");
+	var ele_rcap = document.getElementById("carousel-captionR");
+	var ele_pbar = document.getElementById("carousel-progress-bar");
+	var carousel_eles = [ ele_img, ele_lcap, ele_rcap ];
+
+	// fade out
+	carousel_eles.forEach(ele => ele.classList.remove("fadein"));
+	carousel_eles.forEach(ele => ele.classList.add("fadeout"));
+
+	// wait for element to disappear
+	while (getComputedStyle(ele_img).opacity != 0) { await new Promise(r => setTimeout(r, 25)); }
+
+	// update
+	ele_img.src = gallery_data.images[gallery_data.idx][0];
+	ele_lcap.innerHTML = gallery_data.images[gallery_data.idx][1];
+	ele_rcap.innerHTML = gallery_data.images[gallery_data.idx][2];
+
+	// wait for image to load
+	while (!ele_img.complete) { await new Promise(r => setTimeout(r, 25)); }
+
+	// fade in
+	carousel_eles.forEach(ele => ele.classList.add("fadein"));
+	carousel_eles.forEach(ele => ele.classList.remove("fadeout"));
+	ele_pbar.style.width = `${100 * (gallery_data.idx + 1) / gallery_data.images.length}%`
+
+	// reset the interval so that the image doesn't immediately change on render
+	window.clearInterval(galleryAutoIntervalID);
+	galleryAutoIntervalID = window.setInterval(carouselNextImage, 7000);
+}
+
+// renders the next image in the carousel
+async function carouselNextImage() {
+	
+	// increment and render
+	gallery_data.idx = (gallery_data.idx + 1) % gallery_data.images.length;
+	carouselRenderImage();
+}
+
+// renders the previous image in the carousel
+function carouselPreviousImage() {
+
+	// decrement and render
+	gallery_data.idx = gallery_data.idx == 0 ? gallery_data.images.length - 1 : gallery_data.idx - 1;
+	carouselRenderImage();
+}
+
+// an in-joke stemming from the use of my head.jpg as a placeholder for exco headshots
+function toggleSuperSecretMode() {
+	var portraits = Array.from(document.getElementsByClassName("portrait"));
+	if (portraits[0].alt == "super secret") {
+		portraits.forEach(function (portrait) { portrait.alt = ""; });
+		loadExcoList();
+	} else {
+		portraits.forEach(function (portrait) {
+			portrait.src = "assets/img/exco23/head.jpg";
+			portrait.alt = "super secret";
+		});
+	}
+}
